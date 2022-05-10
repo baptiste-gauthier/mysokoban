384efaeef6691d9fa34fb02aea7705edea089933
Baptiste <baptiste.gauthier@synergiefamily.com>
2022-05-10 15:37:05 +0200

    generation map a peu pres ok

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 from player import Player
+from map import Map
 
 class Game:
     def __init__(self) -> None:
         self.player = Player()
+        self.map = Map()
         self.pressed = {}
--- main.py
+++ main.py
@@ -20,27 +20,85 @@ pygame.init()
 #         return myArray
 
 size = width, height = 1080, 520
-
-pygame.display.set_caption("Zelda Sokoban")
+pygame.display.set_caption("A link's Sokoban")
 screen = pygame.display.set_mode(size)
 
-# link_walking = pygame.image.load("assets/link_walking.gif")
-chicken = pygame.image.load("assets/chicken.gif")
-rock = pygame.image.load("assets/rock.png")
-tree = pygame.image.load("assets/tree.png")
+#0
 grass = pygame.image.load("assets/grass.png")
+grass = pygame.transform.scale(grass, (40, 40))
+#1
+rock = pygame.image.load("assets/rock.png")
+rock = pygame.transform.scale(rock, (40, 40))
+#2
+chicken = pygame.image.load("assets/Chicken.gif")
+chicken = pygame.transform.scale(chicken, (40, 40))
+#3
+link_walking = pygame.image.load("assets/link_walking.gif")
+link_walking = pygame.transform.scale(link_walking, (40, 40))
+#4
+tree = pygame.image.load("assets/tree.png")
+tree = pygame.transform.scale(tree, (40, 40))
+#5
+mushroom = pygame.image.load("assets/mushroom.png")
+mushroom = pygame.transform.scale(mushroom, (40, 40))
+#6
+water = pygame.image.load("assets/water.png")
+water = pygame.transform.scale(water, (40, 40))
+#7
+rupee = pygame.image.load("assets/rupee.png")
+rupee = pygame.transform.scale(rupee, (40, 40))
+
+
 
 #instance du player
 
 game = Game()
+myMap = game.map.generateMap()
+print(myMap[0])
+# print(len(myMap))
+# print(myMap[1][0])
+# print(myMap[2][0])
 
-# myGround = Ground(20,12)
-# print(myGround.buildGround())
+i = 0
+while i < len(myMap):
+    # print(i,"index")
+    a = 0
+    while a < len(myMap[i]):
+        if myMap[i][a] == 0:
+            screen.blit(grass, (grass.get_height() * i, grass.get_width() * a))
+        elif myMap[i][a] == 1:
+            screen.blit(rock, (rock.get_height() * i,rock.get_width() * a))
+        elif myMap[i][a] == 2:
+            screen.blit(chicken, (chicken.get_height() * i,chicken.get_width() * a))
+        elif myMap[i][a] == 3:
+            screen.blit(link_walking, (link_walking.get_height() * i,link_walking.get_width() * a))
+        elif myMap[i][a] == 4:
+            screen.blit(tree, (tree.get_height() * i,tree.get_width() * a))
+        elif myMap[i][a] == 5:
+            screen.blit(mushroom, (mushroom.get_height() * i,mushroom.get_width() * a))
+        elif myMap[i][a] == 6:
+            screen.blit(water, (water.get_height() * i,water.get_width() * a))
+        elif myMap[i][a] == 7:
+            screen.blit(rupee, (rupee.get_height() * i,rupee.get_width() * a))
+        a += 1
+    i += 1
+
+
+# a = 0
+# b = 0
+# for i in myMap:
+#     print(i[0])
+#     if i[a] == 0:
+#         screen.blit(grass, (0,grass.get_height()*a))
+#     elif i[a] == 1:
+#         screen.blit(rock, (0,rock.get_height()*a))    
+#     a = a + 1
 
 while 1:
 
-    #injection de l'mage du personnage 
+    #injection de l'image du personnage 
     screen.blit(game.player.image, game.player.rect)
+    
 
     if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < screen.get_width():
         game.player.moveRight()
@@ -54,8 +112,9 @@ while 1:
     elif game.pressed.get(pygame.K_DOWN):
         game.player.moveBottom()
 
-    print(game.player.rect.x)
-    print(game.player.rect.y)
+    # print(game.player.rect.x)
+    # print(game.player.rect.y)
+    # print(game.map.generateMap())
 
     
 
